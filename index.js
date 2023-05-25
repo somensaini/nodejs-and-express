4cc44904a8783c2643a153b15170d1d61ce6791f
Somen Saini <sainisomen@gmail.com>
2023-05-25 18:02:32 -0400

    exercise 3.1 complete

--- index.js
+++ index.js
@@ -20,11 +20,36 @@ let notes = [
     important: true  
   }
 ]
-  
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+// home route
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+// api/notes route
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
@@ -38,6 +63,11 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end()
   }
   response.json(note)
+})
+
+// api/persons route
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
 const generateId = () => {
